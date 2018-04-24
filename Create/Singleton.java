5f053a7fa08693ec41ffe269909e67e86898a3fb
MaugerWu <34430421+MaugerWu@users.noreply.github.com>
2018-04-24 23:29:18 +0800

    Update Singleton Pattern

--- Create/Singleton.java
+++ Create/Singleton.java
@@ -44,12 +44,13 @@ import java.util.Map;
  * 
  * 饿汉式写法
  */
-public class Singleton {
-	
+public class Singleton
+{
 	private static Singleton instance = new Singleton();
 	private Singleton() {}
 	
-	public static Singleton getInstance() {
+	public static Singleton getInstance()
+	{
 		return instance;
 	}
 }
@@ -61,13 +62,15 @@ public class Singleton {
  * @date 2018年4月19日  
  * @version 1.0
  */
-class Singleton0 {
-
+class Singleton0
+{
 	private static Singleton0 instance;
 	private Singleton0() {}
 	
-	public static Singleton0 getInstance() {
-		if (instance == null) {
+	public static Singleton0 getInstance()
+	{
+		if (instance == null)
+		{
 			instance = new Singleton0();
 		}
 		return instance;
@@ -81,13 +84,15 @@ class Singleton0 {
  * @date 2018年4月19日  
  * @version 1.0
  */
-class Singleton1 {
-
+class Singleton1
+{
 	private static Singleton1 instance;
 	private Singleton1() {}
 	
-	public static synchronized Singleton1 getInstance() {
-		if (instance == null) {
+	public static synchronized Singleton1 getInstance()
+	{
+		if (instance == null)
+		{
 			instance = new Singleton1();
 		}
 		return instance;
@@ -101,16 +106,16 @@ class Singleton1 {
  * @date 2018年4月19日  
  * @version 1.0
  */
-class Singleton2 {
-	
-	private static class SingletonHolder {
-		
+class Singleton2
+{
+	private static class SingletonHolder
+	{
 		private static final Singleton2 INSTANCE = new Singleton2();
 	}
-	
 	private Singleton2() {}
 	
-	public static Singleton2 getInstance() {
+	public static Singleton2 getInstance()
+	{
 		return SingletonHolder.INSTANCE;
 	}
 }
@@ -128,7 +133,8 @@ class Singleton2 {
  * @date 2018年4月19日  
  * @version 1.0
  */
-enum Singleton3 {
+enum Singleton3
+{
 	INSTANCE;
 	private Singleton3() {}
 	
@@ -151,14 +157,18 @@ class DBConnection {}
  * @date 2018年4月19日  
  * @version 1.0
  */
-enum DataSourceEnum {
+enum DataSourceEnum
+{
     DATASOURCE;
     private DBConnection conn;
-    private DataSourceEnum() {
+    
+    private DataSourceEnum()
+    {
         conn = new DBConnection();
     }
     
-    public DBConnection getConnection() {
+    public DBConnection getConnection()
+    {
         return conn;
     }
 }
@@ -170,9 +180,10 @@ enum DataSourceEnum {
  * @date 2018年4月19日  
  * @version 1.0
  */
-class enumTest {
-	
-	public static void main(String[] args) {
+class enumTest
+{
+	public static void main(String[] args)
+	{
 		DBConnection conn1 = DataSourceEnum.DATASOURCE.getConnection();
 		DBConnection conn2 = DataSourceEnum.DATASOURCE.getConnection();
 		System.out.println(conn1 == conn2); // 返回 true
@@ -181,21 +192,27 @@ class enumTest {
 
 /**
  * 双重校验锁
- * 	第一个if (instance == null)是为了解决 Singleton1 中的效率问题，只有 instance 为 null 的时候，才进入 synchronized 的代码段。
+ * 	第一个if (instance == null)是为了解决 Singleton1 中的效率问题，只有 instance 为 null 的时候，
+ * 	才进入 synchronized 的代码段。
  * 	第二个if (instance == null)则是跟 Singleton1 一样，是为了防止可能出现多个实例的情况。
+ * 
  * @author Mauger
  * @date 2018年4月19日  
  * @version 1.0
  */
-class Singleton4 {
-	
+class Singleton4
+{
 	private static Singleton4 instance;
 	private Singleton4() {}
 	
-	public static Singleton4 getInstance() {
-		if (instance == null) {
-			synchronized(Singleton4.class) {
-				if (instance == null) {
+	public static Singleton4 getInstance()
+	{
+		if (instance == null)
+		{
+			synchronized(Singleton4.class)
+			{
+				if (instance == null)
+				{
 					instance = new Singleton4();
 				}
 			}
@@ -209,21 +226,25 @@ class Singleton4 {
  * volatile
  * 	volatile 关键字的一个作用是禁止指令重排;
  * 	volatile 阻止的不是 singleton = new Singleton()这句话内部[1-2-3]的指令重排，
- * 而是保证了在一个写操作（[1-2-3]）完成之前，去执行读操作（if (instance == null)）。
+ * 	而是保证了在一个写操作（[1-2-3]）完成之前，去执行读操作（if (instance == null)）。
  * 
  * @author Mauger
  * @date 2018年4月19日  
  * @version 1.0
  */
-class Singleton5 {
-	
+class Singleton5
+{
 	private static volatile Singleton5 instance;
 	private Singleton5() {}
 	
-	public static Singleton5 getInstance() {
-		if (instance == null) {
-			synchronized(Singleton5.class) {
-				if (instance == null) {
+	public static Singleton5 getInstance()
+	{
+		if (instance == null)
+		{
+			synchronized(Singleton5.class)
+			{
+				if (instance == null)
+				{
 					instance = new Singleton5();
 				}
 			}
@@ -235,32 +256,37 @@ class Singleton5 {
 
 /**
  * 登记式单例写法
- * 	登记式单例实际上维护了一组单例类的实例，将这些实例存放在一个Map（登记薄）中，对于已经登记过的实例，则从Map直接返回，对于没有登记的，则先登记，然后返回。
+ * 	登记式单例实际上维护了一组单例类的实例，将这些实例存放在一个 Map（登记薄）中，对于已经登记过的实例，则从 Map 直接返回，
+ * 	对于没有登记的，则先登记，然后返回。
  * 
  * @author Mauger
  * @date 2018年4月19日  
  * @version 1.0
  */
-class RegisterSingleton {
-	
+class RegisterSingleton
+{
 	private static Map<String, RegisterSingleton> map = new HashMap<String, RegisterSingleton>();
-	static {
+	static
+	{
 		map.put(RegisterSingleton.class.getName(), new RegisterSingleton());
 	}
-	
 	protected RegisterSingleton() {}
 	
 	@SuppressWarnings("unchecked")
-	public static <T>T getInstance(T t) {
-		
+	public static <T>T getInstance(T t)
+	{
 		String className = t.getClass().getName();
-		if (RegisterSingleton.class.getName() == null) {
-			try {
+		if (RegisterSingleton.class.getName() == null)
+		{
+			try
+			{
 				Constructor<?> constructor = Class.forName(className).getDeclaredConstructor();
 				constructor.setAccessible(true);
 				map.put(className, (RegisterSingleton)constructor.newInstance());
-			} catch (Exception e) {
+			} catch (Exception e)
+			{
 				className = RegisterSingleton.class.getName();
+				e.printStackTrace();
 			}
 		}
 		return (T)map.get(className);
@@ -274,32 +300,38 @@ class RegisterSingleton {
  * @date 2018年4月19日  
  * @version 1.0
  */
-class RegSingleton {
-	
+class RegSingleton
+{
 	private static Map<String, RegSingleton> regMap = new HashMap<String, RegSingleton>();
-	static {
+	static
+	{
 		RegSingleton instance = new RegSingleton();
 		regMap.put(instance.getClass().getName(), instance);
 	}
 	protected RegSingleton() {}
 	
-	public static RegSingleton getInstance(String className) {
-		
-		if (className == null) {
+	public static RegSingleton getInstance(String className)
+	{
+		if (className == null)
+		{
 			className = RegSingleton.class.getName();
 		}
-		
-		if (regMap.get(className) == null) {
-			try {
+		if (regMap.get(className) == null)
+		{
+			try
+			{
 				regMap.put(className, (RegSingleton) Class.forName(className).newInstance());
-			} catch (Exception e) {
+			} catch (Exception e)
+			{
 				className = RegisterSingleton.class.getName();
+				e.printStackTrace();
 			}
 		}
 		return regMap.get(className);
 	}
 	
-	public String getStr() {
+	public String getStr() 
+	{
 		return "Hello World!";
 	}
 }
